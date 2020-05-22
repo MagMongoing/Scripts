50688515115acbbe0416f0cfa8402b2f6a185aa1
MagMongoing <magbon6@gmail.com>
2020-05-22 19:05:40 +0800

    delete sys

--- vip_detect.py
+++ vip_detect.py
@@ -2,13 +2,11 @@
 # -*- coding:utf-8 -*-
 import configparser
 import pymysql
-import sys
 import subprocess
 import redis
 import time
 
 from dingtalkchatbot.chatbot import DingtalkChatbot
-
 
 
 config = configparser.ConfigParser()
@@ -19,11 +17,13 @@ password = config.get('mysql', 'password')
 port = int(config.get('mysql', 'port'))
 robot = config.get('data-center', 'test')
 
+
 def send_msg_robot(msg):
     ding = DingtalkChatbot(robot)
-    ding.send_text(msg=msg,is_at_all=False)
+    ding.send_text(msg=msg, is_at_all=False)
 
-r = redis.Redis(host='127.0.0.1',port=6379,db=1)
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=1)
 
 conn = pymysql.connect(host=host, user=user, password=password, port=port, db='archery', charset='utf8mb4')
 cursor = conn.cursor()
@@ -35,15 +35,15 @@ result = cursor.fetchall()
 for host in result:
     time.sleep(0.2)
     cmd = "ping -c 1 " + host[0]
-    vip_status = subprocess.call(cmd,shell=True)
+    vip_status = subprocess.call(cmd, shell=True)
     vip_cur_status = 1 if vip_status == 0 else 0
-    key = host[1].split('-')[0] + '-vip-' +  host[0]
+    key = host[1].split('-')[0] + '-vip-' + host[0]
     print(key)
-    if r.get(key) == None:
+    if r.get(key) is None:
         r.set(key, vip_cur_status)
         if vip_cur_status == 0:
-                msg = key + ' is Down!'
-                send_msg_robot(msg)
+            msg = key + ' is Down!'
+            send_msg_robot(msg)
         else:
             msg = key + ' is monitored'
             send_msg_robot(msg)
